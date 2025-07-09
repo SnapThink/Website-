2ab56dd7c9223cd06d6dcc78c846a5b3ced423cf
SnapThink <snapthinkllm@gmail.com>
2025-07-09 15:26:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'SnapThink',
-  description: 'Created with v0',
+  description: 'Platform for LLMs',
   generator: 'Snapthink',
 }
 
